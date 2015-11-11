2f582d673f86efeb11c46371ac2d30d6a1a4c19f
zhuyingda <383329144@qq.com>
2015-11-11 11:18:37 +0800

    add comment

--- src/utils/truncate/truncate.js
+++ src/utils/truncate/truncate.js
@@ -9,6 +9,7 @@
  * @desc 根据配置识别中英文，按长度截取字符串，兼容ie6
  * @param {string} str 待切割字符
  * @param {number} len 长度限制，以英文或半角符号为1个单位长度，中文占2个
+ * @param {boolean} ellipsis 超出字长后是否显示省略号，此参数可以不填
  */
 
 function index(str, len, ellipsis) {
